45675a89f9fa93be1c12853785ff413718486122
ChenYXin <1224670105@qq.com>
2017-12-11 18:02:29 +0800

    deep night

---
+++ app/src/main/java/com/donkor/deepnight/SplashActivity.kt
@@ -0,0 +1,53 @@
+package com.donkor.deepnight
+
+import android.graphics.Typeface
+import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_splash.*
+import java.lang.ref.WeakReference
+
+// *************************************************
+// Handler:跳转至不同页面
+// *************************************************
+private val SWITCH_MAINACTIVITY = 1000//跳转到主界面
+private val TAG: String? = "SplashActivity"
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //设置全屏
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash)
+        initView()
+        MyHandler(this).sendEmptyMessageDelayed(SWITCH_MAINACTIVITY, 1000)
+    }
+
+    private class MyHandler(activity: SplashActivity) : Handler() {
+        private val mActivity: WeakReference<SplashActivity> = WeakReference(activity)
+
+        override fun handleMessage(msg: Message) {
+            if (mActivity.get() == null) {
+                return
+            }
+//            val activity = mActivity.get()
+            when (msg.what) {
+                SWITCH_MAINACTIVITY -> {
+                    Log.e(TAG, "SplashActivity --- switch_MainActivity")
+                }
+                else -> {
+                }
+            }
+        }
+    }
+
+    private fun initView() {
+        val font: Typeface = Typeface.createFromAsset(this.assets, "fonts/Lobster-1.4.otf")
+        tv_splash_instruction.typeface = font
+        tv_splash_instruction.text = getString(R.string.splash_activity_splash_instruction)
+    }
+}
